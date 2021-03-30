1d3f12a0cb8f1bd751c0fc7c7f0320c66f40bd90
Sebastien <slatouche578@gmail.com>
2021-03-30 19:38:34 +1000

    Added Imports

--- WebApi_EF_Test/Data/ITestRepo.cs
+++ WebApi_EF_Test/Data/ITestRepo.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using WebApi_EF_Test.Models;
+
 namespace WebApi_EF_Test.Data
 {
     public interface ITestRepo
--- WebApi_EF_Test/Data/MockTestRepo.cs
+++ WebApi_EF_Test/Data/MockTestRepo.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using WebApi_EF_Test.Models;
+
 namespace WebApi_EF_Test.Data
 {
     public class MockTestRepo : ITestRepo
@@ -11,7 +14,7 @@ namespace WebApi_EF_Test.Data
                 new TestModel{Id=2, TestString="Test String 2", TestOtherString="Other Test String 2", TestInt=102},
                 new TestModel{Id=3, TestString="Test String 3", TestOtherString="Other Test String 3", TestInt=103},
                 new TestModel{Id=4, TestString="Test String 4", TestOtherString="Other Test String 4", TestInt=104}
-            }
+            };
 
             return testModels;
         }
