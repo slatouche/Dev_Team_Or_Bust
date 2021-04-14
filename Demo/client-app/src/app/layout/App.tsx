b68a2b8041b6b9bb92b103eacc08c2561b928f3d
Sebastien <slatouche578@gmail.com>
2021-04-14 22:10:13 +1000

    Create/Edit form now displaying based on buttons clicked

--- Demo/client-app/src/app/layout/App.tsx
+++ Demo/client-app/src/app/layout/App.tsx
@@ -7,8 +7,10 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 
 function App() {
 
+  //States
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
@@ -18,21 +20,34 @@ function App() {
 
   function handleSelectActivity(id: string) {
     setSelectedActivity(activities.find(x => x.id === id))
+    setEditMode(false);
   }
 
   function handleCancelSelectActivity() {
     setSelectedActivity(undefined);
   }
 
+  function handleFormOpen(id?: string) {
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
+
   return (
     <>
-      <NavBar />
+      <NavBar openForm={handleFormOpen} />
         <Container style={{marginTop: '7em'}}>
           <ActivityDashboard 
             activities={activities}
             selectedActivity={selectedActivity}
             selectActivity={handleSelectActivity}
             cancelSelectActivity={handleCancelSelectActivity}
+            editMode={editMode}
+            openForm={handleFormOpen}
+            closeForm={handleFormClose}
           />
         </Container>
     </>
